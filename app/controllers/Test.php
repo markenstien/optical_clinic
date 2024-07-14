1570a5184acdc9563f77c3e1bab93304591cc1ec
Mark Angelo Gonzales <markangelogonzalespersonal@gmail.com>
2024-07-14 23:20:55 +0800

    add fix;

--- app/controllers/Test.php
+++ app/controllers/Test.php
@@ -23,4 +23,33 @@
 
 			// _mail('chromaticsoftwares@gmail.com' , "Verify Account" , $email_body);
 		}
+
+		public function demo() {
+			require_once 'HTTP/Request2.php';
+			$request = new HTTP_Request2();
+			$request->setUrl('https://y3y6dd.api.infobip.com/sms/2/text/advanced');
+			$request->setMethod(HTTP_Request2::METHOD_POST);
+			$request->setConfig(array(
+				'follow_redirects' => TRUE
+			));
+			$request->setHeader(array(
+				'Authorization' => 'App ********************************-********-****-****-****-********665a',
+				'Content-Type' => 'application/json',
+				'Accept' => 'application/json'
+			));
+			$request->setBody('{"messages":[{"destinations":[{"to":"639063387451"}],"from":"VVDOPTICL","text":"Congratulations on sending your first message.\\nGo ahead and check the delivery report in the next step."}]}');
+			try {
+				$response = $request->send();
+				if ($response->getStatus() == 200) {
+					echo $response->getBody();
+				}
+				else {
+					echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+					$response->getReasonPhrase();
+				}
+			}
+			catch(HTTP_Request2_Exception $e) {
+				echo 'Error: ' . $e->getMessage();
+			}
+					}
 	}
